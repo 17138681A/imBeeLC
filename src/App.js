50f67c7db511b9cda69c8229d96c36f88f227ab3
20627270 <william.tsang24@gmail.com>
2021-06-17 10:40:09 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import logo from './logo_imbeeo.png';
 import './App.css';
+import { Widget, renderCustomComponent } from 'react-chat-widget';
+import './styles.css';
+
+class welcomeForm extends Component{
+	render(){
+		return <img src={logo} className="App-logo" alt="logo" />
+	}
+}
 
 function App() {
+	
+	
+		renderCustomComponent(welcomeForm);
+	
+
+	const handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming!` , newMessage);
+    // Now send the message throught the backend API
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <b>
+		  Live Chat Demo
+        </b>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          imBee
         </a>
       </header>
+	  <Widget 
+	  subtitle = "imBee Team"
+	  handleNewUserMessage={handleNewUserMessage}
+	  />
     </div>
   );
 }
+
 
 export default App;
